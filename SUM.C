8254a6722a05830794ab71116a930247be3bfcd7
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-02-18 11:04:18 +0300

    coding with c

---
+++ SUM.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// #include <conio.h>
+main()
+{
+    int a,b, sum;
+    // clrscr();
+    printf("Enter two numbers: ");
+    scanf("%d,%d,&a,&b");
+sum = a + b;
+printf("sum=%d",sum);
+
+// getch();
+
+}
