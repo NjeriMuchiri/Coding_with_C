cbdaf9a090215453a56fabec0dbb53005cac6ef0
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-02-23 15:29:33 +0300

    loops

--- loop_foring.c
+++ loop_foring.c
@@ -26,13 +26,5 @@
 // program that prints a multiplication chart
 #include<stdio.h>
 
-int main()
-{
-    int i,j;
-    for(i=1;i<10; i++)
-    {
-        for(j=1; j<10; j++)
-        printf("%3d", i*j);
-        printf("\n");
-    }
-}
+
+
