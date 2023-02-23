6a7390998336e1ec7ab4f21c2a40e7297ca76538
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-02-23 15:52:57 +0300

    looping

--- loop_foring.c
+++ loop_foring.c
@@ -10,7 +10,8 @@
 // // Program that gets the temperatures of week days and the verage for that week
 // #include<stdio.h>
 
-// int main(){
+// int main()
+// {
 //     int count = 0;
 //     float num = 0, sum = 0, avg = 0;
 //     for(count = 0; count < 7; count++)
@@ -24,7 +25,55 @@
 // }
 
 // program that prints a multiplication chart
-#include<stdio.h>
+// // while loop
+// #include<stdio.h>
+//  int main()
+//  {
+//     int i = 1;
+//     while(i<=5)
+//     {
+//         int j = 1;
+//         while(j<=5)
+//         {
+//         printf("%d * %d = %d\n",i,j, i*j);
+//         j++;
+//         }
+//         i++;
+//         printf("\n");
+//     }
+//     return (0);
+//  }
+// for loop
+ #include <stdio.h>
 
+ int main()
+{
+    for (int i = 1; i <= 5; i++)
+    {
+         for (int j = 1; j <=5; j++)
+        printf("%d * %d = %d\n",i,j, i*j);
+  }
+ return (0);
+}
 
+// do while loop
+// #include <stdio.h>
 
+// int main()
+// {
+//     int i = 1;
+//     do 
+//     {
+//         int j =1;
+//         do
+//         {
+//             printf("%d * %d = %d\n", i,j , i * j);
+//             j++;
+//         } 
+//         while(j <=5);
+//         i++;
+//         printf("\n");
+//     }
+//     while(i <= 5);
+//     return (0);
+// }
