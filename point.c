1af11e23b9bc5d26db4e8d462bf5dfa239ab6436
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-02-27 11:40:13 +0300

    Added a new program

--- point.c
+++ point.c
@@ -1,16 +1,35 @@
-#include<stdio.h>
-/**
- * main - using sizeof to dynamically determine the size of types char, int, and float
- * Return: Always 0.
-*/
+// #include<stdio.h>
+// /**
+//  * main - using sizeof to dynamically determine the size of types char, int, and float
+//  * Return: Always 0.
+// */
 
+// int main(void)
+// {
+//    int n;
+   
+//    printf("Size of type 'char' on my computer: %lu bytes\n", sizeof(char));
+//    printf("Size of type 'int' on my computer: %lu bytes\n", sizeof(int));
+//    printf("Size of type 'float' on my computer: %lu bytes\n", sizeof(float));
+//    printf("Size of type of my variable n on my computer: %lu bytes\n", sizeof(n));
+//    return (0);
+// }
+
+// Address of the unary operator
+#include <stdio.h>
+
+/**
+ * main - adresses of variable
+ * 
+ * Return: Always 0
+*/
 int main(void)
 {
-   int n;
-   
-   printf("Size of type 'char' on my computer: %lu bytes\n", sizeof(char));
-   printf("Size of type 'int' on my computer: %lu bytes\n", sizeof(int));
-   printf("Size of type 'float' on my computer: %lu bytes\n", sizeof(float));
-   printf("Size of type of my variable n on my computer: %lu bytes\n", sizeof(n));
-   return (0);
+    char c;
+    int n;
+
+    printf("Address of variable 'c': %p\n", &c); //%p to print addresses i.e the values of pointers 
+    printf("Address of variable 'n': %p\n", &n);
+    return (0);
 }
+
