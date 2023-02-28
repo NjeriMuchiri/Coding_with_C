1713f91a54999ffae8dcd45299e95c300148ef72
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-02-28 18:06:32 +0300

    integer limits

--- sizing.c
+++ sizing.c
@@ -1,7 +1,21 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//     printf("%ld\n", sizeof(int)); //size of is a unary operator
+//     return 0;
+// }
+
+//limits.h header
+
 #include <stdio.h>
+#include <limits.h>
 
 int main()
 {
-    printf("%ld\n", sizeof(int));
+    int var1 = INT_MIN;
+    int var2 = INT_MAX;
+
+    printf("range of signed integer is from: %d to %d\n", var1, var2);
     return 0;
 }
