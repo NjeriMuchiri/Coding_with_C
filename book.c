a8c21aceec6331731d4c5ecc30c4a027af377aa3
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-03-03 15:23:45 +0300

    book programs

--- book.c
+++ book.c
@@ -1,12 +1,33 @@
 // printing arrays in the reverse 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int i,n;
+//     printf("Enter the value of N: ");
+//     scanf("%d", &n);
+//     for(i = n; i >= 1; i--)
+//     printf("%d", i);
+//     printf("\b\n"); //To remove the comma(,) printed at the last */
+// }
+
+//fibonacci
 #include <stdio.h>
 
 int main()
 {
-    int i,n;
+    int prev,next,cnt,sum,n;
     printf("Enter the value of N: ");
     scanf("%d", &n);
-    for(i = n; i >= 1; i--)
-    printf("%d", i);
-    printf("\b\n"); //To remove the comma(,) printed at the last */
+    prev = 0;
+    next=cnt=1;
+    while(cnt <= n)
+    {
+        printf("%d,",next);
+        sum = prev + next;
+        prev = next;
+        next = sum;
+        cnt++;
+    }
+printf("\b\n");
 }
