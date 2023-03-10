0b677aa2759c7086608cae62cdf97aabdf33233f
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-03-10 17:14:29 +0300

    argc and argv

--- arrg.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-// argc - argument count : contans the number of arguments passed to the program
-
-int main (int argc, char *argv[])
-{
-    return 0;
-}
---
+++ myprog.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+//argc - argument count and passed to the program
+//argv - argument vector whereby argv is a one dimensional array of strings
+int main(int argc, char *argv[])
+{
+ return 0;
+}
