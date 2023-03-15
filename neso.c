8f8c41c47f4d75722efc13802653b00c8fa8ae31
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-03-15 16:44:37 +0300

    functions

--- neso.c
+++ neso.c
@@ -2,27 +2,47 @@
 // #include <stdio.h>
 // int main()
 // {
-//     char var = 65;
+//     char var = 128;
 //     printf("%c\n", var);
 //     return 0;
 // }
 
 //smiley
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     int i, j;
+
+//     for (i = 0; i <= 10; i++)
+//     {
+//         for (j = 0; j <= 5; j++)
+//         {
+//             printf("%s", "\u263A");
+//         }
+
+//         printf("\n");
+//     }
+
+//     return 0;
+// }
+// #include <stdio.h>
+// int main()
+// {
+// float var1 = 3.131592653589793113456;
+// double var2 = 3.131592653589793113456;
+// long double var3 = 3.131592653589793113456;
+
+// printf("%ld\n", sizeof(float));
+// printf("%ld\n", sizeof(double));
+// printf("%ld\n", sizeof(long double));
+// printf("%.16f\n", var1);
+// printf("%.16f\n",var2 );
+// printf("%.21Lf\n",var3);
+// }
 #include <stdio.h>
-
-int main(void)
+int main()
 {
-    int i, j;
-
-    for (i = 0; i <= 10; i++)
-    {
-        for (j = 0; j <= 5; j++)
-        {
-            printf("%s", "\u263A");
-        }
-
-        printf("\n");
-    }
-
-    return 0;
+    printf("%d\n", printf("%s\n", "Hello World!"));
+    return (0);
 }
