c4bbcd9b11435e39fbd3c5497a685d9d770d5353
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-04-05 13:25:19 +0300

    singly linked llists

--- linkedlist.c
+++ linkedlist.c
@@ -1,19 +1,44 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// struct node
+// {
+// int data;
+// struct node *link;
+// };
+// int main()
+// {
+//     struct node *head = NULL;
+//     head = (struct node *)malloc(sizeof(struct node));
+
+//     head->data = 45;
+//     head->link = NULL;
+
+//     struct node *current = malloc(sizeof(struct node));
+//     current->data = 98;
+//     current->link=NULL;
+//     head->link = current;
+
+//     current = malloc(sizeof(struct node));
+//     current->data = 3;
+//     current->link = NULL;
+
+//     head->link->link = current;
+
+//     printf("%d\n,%d\n", head->data,current->data);
+//     return 0;
+// }
+
+//traversing a cngle link list
 #include <stdio.h>
 #include <stdlib.h>
 
 struct node
 {
-int data;
-struct node *link;
+    int data;
+    struct node *link;
 };
 int main()
 {
-    struct node *head = NULL;
-    head = (struct node *)malloc(sizeof(struct node));
-
-    head->data = 45;
-    head->link = NULL;
-
-    printf("%d\n", head->data);
-    return 0;
+    count_of_nodes(head);
 }
