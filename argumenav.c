8ee7908dfacb4e8c221d8aaf911f9e4c51e37319
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-04-14 00:04:20 +0300

    arguments on command line

--- argumenav.c
+++ argumenav.c
@@ -3,13 +3,20 @@
 // @ac: number of argumets in av
 // @av: array of strings(arguments)
 
-int main(int ac, char **av)
+// int main(int ac, char **av)
+// {
+//     int i;
+//     for(i = 0; i < ac; i++)
+//     {
+//         (void) ac;
+//         printf("%s\n", av[i]);
+//     }
+//     return (0);
+// }
+int main(int argc, char *argv[])
 {
     int i;
-    for(i = 0; i < ac; i++)
-    {
-        (void) ac;
-        printf("%s\n", av[i]);
-    }
-    return (0);
+    for (i= 0; i < argc; i++)
+    printf("%s\n", argv[i]);
+    return 0;
 }
