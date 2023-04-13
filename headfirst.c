af7b5d06bee4ddd90bf58ddae7acecbd2c886c69
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-04-13 16:35:28 +0300

    pointers init

--- headfirst.c
+++ headfirst.c
@@ -10,10 +10,47 @@
 //     puts("C rocks");
 //     return 0;
 // }
+// int main()
+// {
+//     char ex[20];
+//     puts("Enter a previous job: ");
+//     scanf("%19s", ex);
+//     printf("Dear %s. \n\n\tYou're history.\n", ex);
+// }
+// int main()
+// {
+//     char name[16];
+//     puts("Enter your name: ");
+//     fgets(name, 8, stdin);
+//     do
+//     {
+//         printf("Hi %s", name);
+//     }
+//     while (!name);
+//     {
+//         puts("Hi");
+//     }
+//     return 0;
+// }
+// int main()
+// {
+//     int* pc, c;
+//     c = 5;
+//     pc = &c;
+//     printf("%d\n", *pc);
+//     printf("C is stored at %p\n: ", &c);
+// }
+void  go_south_east(int *lat, int *lon)
+{
+    *lat -= 1;
+    *lon += 1;
+}
 int main()
 {
-    char ex[20];
-    puts("Enter a previous job: ");
-    scanf("%19s", ex);
-    printf("Dear %s. \n\n\tYou're history.\n", ex);
+    int latitude = 32;
+    int longitude = -64;
+
+    go_south_east(&latitude, &longitude);
+    printf("Avast! Now at: [%i, %i]\n", latitude, longitude);
+    return 0;
 }
