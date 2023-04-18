619852978c3d738d3bc87087bb7c9597d7e30786
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-04-18 11:34:04 +0300

    execve processes

--- argumenav.c
+++ argumenav.c
@@ -3,20 +3,21 @@
 // @ac: number of argumets in av
 // @av: array of strings(arguments)
 
-// int main(int ac, char **av)
-// {
-//     int i;
-//     for(i = 0; i < ac; i++)
-//     {
-//         (void) ac;
-//         printf("%s\n", av[i]);
-//     }
-//     return (0);
-// }
-int main(int argc, char *argv[])
+int main(int __attribute__((unused)) ac, char **av)
 {
     int i;
-    for (i= 0; i < argc; i++)
-    printf("%s\n", argv[i]);
-    return 0;
+
+    for(i = 0; av[i]; i++)
+    {
+        // (void) ac;
+        printf("%s\n", av[i]);
+    }
+    return (0);
 }
+// int main(int argc, char *argv[])
+// {
+//     int i;
+//     for (i= 0; i < argc; i++)
+//     printf("%s\n", argv[i]);
+//     return 0;
+// }
